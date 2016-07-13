750f3655e28cd3a200d47c09a43cc7ca8721d609
Francis Rocco <roccofrancis@gmail.com>
2016-07-13 19:49:01 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(array)
+  current_index = 0
+  new_array = []
+  while current_index < array.length
+    new_array << yield(array[current_index])
+    current_index += 1
+  end
+  new_array
+end
